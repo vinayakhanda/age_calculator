3b1c9285f4e187e84cfd40cbaaa8fcfcc48fb34c
Vinayak Handa <86475762+vinayakhanda@users.noreply.github.com>
2021-09-14 16:37:29 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#Age Calculator Made BY Vinayak Handa (18 August 2021)
+print("Welcome To Age Calculator ")
+a = int(input("Enter  Your birth year : "))
+print("Your age is ", 2021-a)
+print("Made by Vinayak Handa \n date = 18 August 2021")
+
